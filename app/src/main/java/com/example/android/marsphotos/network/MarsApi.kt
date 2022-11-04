1f72681cd1e8c7689a82aba7e5d60966a73e462c
Luigi <luigiiorio30@gmail.com>
2022-11-04 10:07:41 +0100

    Finished

--- app/src/main/java/com/example/android/marsphotos/network/MarsApi.kt
+++
@@ -1,6 +0,0 @@
-package com.example.android.marsphotos.network
-
-object MarsApi {
-    val retrofitService : MarsApiService by lazy {
-        retrofit.create(MarsApiService::class.java) }
-}
--- app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -6,18 +6,26 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
+private const val BASE_URL =
+    "https://android-kotlin-fun-mars-server.appspot.com"
+
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
 
-private const val BASE_URL =
-    "https://android-kotlin-fun-mars-server.appspot.com"
-val retrofit = Retrofit.Builder()
+
+private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
+
 interface MarsApiService {
+
     @GET("photos")
-    suspend fun getPhotos(): List<MarsPhoto>
+    suspend fun getPhotos() : List<MarsPhoto>
+}
+
+object MarsApi {
+    val retrofitService: MarsApiService by lazy { retrofit.create(MarsApiService::class.java) }
 }
